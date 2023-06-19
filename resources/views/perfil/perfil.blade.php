e9bef58af52fc447e4e13a70b5f5d43a996310b3
NBahamonde21 <136349577+NBahamonde21@users.noreply.github.com>
2023-06-19 12:45:37 -0400

    Update perfil.blade.php

---
+++ resources/views/perfil/perfil.blade.php
@@ -0,0 +1,87 @@
+<html>
+    
+    
+  <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+     <link rel="stylesheet" href="C:\laragon\www\Pokemon\resources\views\perfil\style.css">
+  </head>
+  <style>
+    *{
+      margin: 0px;
+      padding: 0px;
+    }
+    .container-perfil {
+    
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 300px;
+    margin-left: 350px;
+     
+    }
+    .general {
+      background-image: url("https://www.gratistodo.com/wp-content/uploads/2016/07/pokemon-wallpapers-3.png");
+      background-size: cover;
+      background-position: center;
+      background-repeat: no-repeat;
+      margin:auto;
+      width:auto;
+      height:auto;
+    }
+  </style>
+
+
+  <body>
+    
+
+    <div class="general">
+
+          <div class="container-perfil">
+                <h2>Gestion de perfil</h2>
+                <div class="fotoPerfil">
+                  <img id="imagenPerfil" src="ruta-a-la-imagen-actual.jpg" style="width: 200px; height: 200px;" alt="Foto de perfil">
+                  <input type="file" id="nuevaFotoPerfil" accept="image/*">
+                  
+                </div>
+
+              <div class="col-3">
+                  <label for="formGroupExampleInput" class="form-label">Nombre</label>
+                  <input type="text" class="form-control" id="nombre" placeholder="">
+              </div>
+
+              <div class="col-3">
+                  <label for="formGroupExampleInput" class="form-label">Correo</label>
+                  <input type="text" class="form-control" id="correo" placeholder="">
+              </div>
+
+             <div class="col-3">
+                  <label for="formGroupExampleInput" class="form-label">Contraseña</label>
+                  <input type="password" class="form-control" id="password" placeholder="">
+              </div>
+
+              <button input type="submit" value="Guardar" onclick="cambiarFotoPerfil()" color-style:green;>Cambiar</button>
+        
+          </div>
+
+    </div>  
+    
+    
+
+  <body>
+
+
+  <script>
+    function cambiarFotoPerfil() {
+    var fileInput = document.getElementById('nuevaFotoPerfil');
+    var file = fileInput.files[0];
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+      var imageElement = document.getElementById('imagenPerfil');
+      imageElement.src = e.target.result;
+    };
+
+    reader.readAsDataURL(file);
+    }
+  </script>
+
+</html>
